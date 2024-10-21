2ea8874847b9655944a083cb43acc012dba78c37
juguti <99435694+juguti@users.noreply.github.com>
2024-10-21 12:22:29 +0200

    ch2

---
+++ mot_ch2ENFR.js
@@ -0,0 +1,289 @@
+const mot = {
+    "Supply": "Alimenter",
+    "Plants": "Usines",
+    "Meander": "Méandres",
+    "To wean": "Sevrer",
+    "Foreign / from abroad": "Étranger",
+    "Straight": "Rectiligne",
+    "Greenhouses": "Des serres",
+    "Sustainable": "Durable",
+    "To fear": "Craindre",
+    "To provide somebody with": "Fournir quelque chose à quelqu'un",
+    "Dependent": "Dépendre",
+    "Cut down on": "Réduire",
+    "To arouse": "Susciter",
+    "To praise": "Faire l’éloge de",
+    "Give birth to": "Donner naissance à",
+    "Blueprint": "Projet / Schéma",
+    "To postpone": "Remettre à plus tard",
+    "The shape": "La forme",
+    "Currently": "Actuellement",
+    "Self-sufficient": "Auto-suffisant",
+    "To encounter": "Rencontrer",
+    "Draught": "Sécheresse",
+    "Shortage": "Pénurie",
+    "To address an issue": "S’attaquer à un problème",
+    "To design": "Concevoir",
+    "To wean somebody off": "Sevrer quelqu'un de quelque chose",
+    "Activist": "Militants",
+    "Linger": "Rester",
+    "Sinking": "S'enfoncer",
+    "Beneath": "Sous",
+    "Collapsing": "S’effondrer",
+    "Subsidence": "Affaissement",
+    "Shift": "Changement",
+    "Bearing": "Le support",
+    "Low-lying": "Position basse",
+    "Mitigation": "Atténuation",
+    "Coastal": "Côtière",
+    "Silt": "La vase",
+    "Draining": "Évacuation",
+    "The British economy": "L'économie britannique",
+    "Economics": "L'économie (science)",
+        "Liberalism, free enterprise": "Le libéralisme",
+        "A free trade": "Le libre-échange",
+        "A free-marketeer": "Un partisan du libéralisme",
+        "A planned economy": "Une économie planifiée",
+        "A state-run economy": "Une économie dirigée par l'État",
+        "A state-owned company": "Une entreprise publique",
+        "To intervene": "Intervenir",
+        "State intervention(ism)": "Le dirigisme",
+        "The law of supply and demand": "La loi de l'offre et la demande",
+        "To nationalize": "Nationaliser",
+        "To privatize": "Privatiser",
+        "Capitalism": "Capitalisme",
+        "Figure stuff out": "Comprendre les choses",
+        "Rental": "Location",
+        "Bevy": "Palette",
+        "Feedback": "Retour",
+        "Feedback loops": "Boucles de rétroaction",
+        "Ownership": "Possession",
+        "Agriculture / farming": "L'agriculture",
+        "To produce": "Produire",
+        "A producer": "Un producteur",
+        "Agricultural produce": "Les produits agricoles",
+        "A crop": "Une culture",
+        "To grow corn": "Cultiver le blé/maïs",
+        "The food processing industry": "L'agroalimentaire",
+        "Organic farming": "L'agriculture biologique",
+        "GMOs / genetically modified organisms": "Les OGM",
+        "To breed cattle": "Élever du bétail",
+        "Cattle breeding": "L'élevage du bétail",
+        "A slaughterhouse": "Un abattoir",
+        "Fishing": "La pêche",
+        "A fish factory": "Une conserverie",
+        "Recession / a slump": "Récession / dépression / régression économique",
+        "Set off": "Déclencher",
+        "Plummeted / to slump / to collapse / to plunge / to tumble": "S'effondrer",
+        "Wages": "Salaires",
+        "Pulled down": "Entraîné",
+        "The manufacturing sector / the second sector": "Le secteur secondaire, l'industrie",
+        "An industrialization": "Une industrialisation",
+        "Deindustrialization": "La désindustrialisation",
+        "A factory / plants": "Une usine",
+        "A workshop": "Atelier",
+        "Raw materials": "Les matières premières",
+        "A foreman": "Un contremaître",
+        "To manufacture": "Fabriquer",
+        "To mass produce": "Fabriquer en série",
+        "The output": "La production",
+        "A machine tool": "Une machine-outil",
+        "To operate a machine": "Faire fonctionner une machine",
+        "To meet safety standards": "Respecter les normes de sécurité",
+        "Heavy industry": "L'industrie lourde",
+        "The textile industry": "L'industrie du textile",
+        "The shipbuilding industry": "La construction navale",
+        "Pharmaceuticals": "L'industrie pharmaceutique",
+        "A car maker, a car manufacturer": "Un constructeur automobile",
+        "Car making": "La construction automobile",
+        "To automate": "Automatiser",
+        "To dismantle": "Démanteler",
+        "The service industries": "Les services, le tertiaire",
+        "Physical goods": "Biens physiques",
+        "A company / a firm": "Une entreprise",
+        "A make, a brand": "Une marque",
+        "To set up a company": "Fonder / Monter une entreprise",
+        "To run a business": "Diriger une entreprise",
+        "SMEs, small and medium-sized companies": "Les PME (Petites et Moyennes Entreprises)",
+        "To compete with somebody": "Être en concurrence avec quelqu'un",
+        "A competitor": "Un concurrent",
+        "Competitiveness": "La compétitivité",
+        "Unfair competition": "La concurrence déloyale",
+        "To corner a market": "Accaparer un marché",
+        "To break into a market / a market share": "Percer un marché / Une part de marché",
+        "A merger": "Fusion d'entreprises",
+        "To make profit": "Réaliser des bénéfices",
+        "A loss": "Une perte",
+        "To go bankrupt": "Faire faillite",
+        "Debt": "Une dette",
+        "Whose core": "Dont le noyau / l'essence",
+        "Whether": "Si",
+        "Left-wing policies": "Politiques de gauche",
+        "Case in point": "Exemple",
+        "A staunch": "Fervent",
+        "A downturn, a downswing": "Une phase de baisse",
+        "To fall / to slide / to decrease / to drop": "Baisser / Diminuer",
+        "A slowdown": "Un ralentissement",
+        "To go through a bad patch": "Connaître une période difficile",
+        "The global economic meltdown": "L'effondrement de l'économie mondiale",
+        "To streamline": "Rationaliser",
+        "To downsize": "Dégraisser ses effectifs",
+        "Restructuring": "Restructuration",
+        "To make workers redundant, to lay off workers": "Licencier des salariés",
+        "To cut jobs": "Supprimer des emplois",
+        "Job losses": "Perte d'emplois",
+        "A glimpse": "Un aperçu",
+        "Resurgent": "Renaissant",
+        "Pervasive": "Envahissant",
+        "Seldom": "Rarement",
+        "Faith": "Foi",
+        "Maintains": "Soutient",
+        "The economy is recovering": "L'économie se redresse",
+        "Recovery": "La reprise",
+        "Business is picking up": "Les affaires reprennent",
+        "An upswing in economic growth": "Une remontée de la croissance",
+        "To increase / to go up / to rise": "Augmenter / Élever",
+        "To soar, to rocket": "Monter en flèche",
+        "An exporter": "Un exportateur",
+        "The balance of trade": "La balance commerciale",
+        "Trade figures": "Les chiffres du commerce extérieur",
+        "A deficit / a surplus": "Un déficit / Un excédent",
+        "Tariffs": "Tarifs douaniers",
+        "Trade barriers": "Les barrières douanières",
+        "A trade war": "Une guerre commerciale",
+        "As measured by weighting": "Tel que mesuré par pondération",
+        "Dizzying": "Étourdissant",
+        "Swept": "Balayé",
+        "Swept back": "Est revenu",
+        "Upbeat": "Optimiste",
+        "To boost growth": "Stimuler la croissance",
+        "Austerity measures": "Des mesures d'austérité",
+        "A stimulus plan / package": "Un plan de relance",
+        "A rescue package": "Un plan de sauvetage",
+        "To deregulate": "Déréglementer",
+        "Deregulation": "La déréglementation",
+        "To bail out a company": "Renflouer une entreprise",
+        "A subsidy": "Une subvention",
+        "To subsidize": "Subventionner",
+        "Growth": "La croissance",
+        "Public spending": "Les dépenses publiques",
+        "To balance a budget": "Équilibrer un budget",
+        "Drastic budget cuts": "Réduction budgétaire",
+        "A budget deficit": "Un déficit budgétaire",
+        "A credit-rating agency": "Une agence de notation",
+        "A tax rise": "Une augmentation des impôts",
+        "Investment": "L'investissement",
+        "Consumer spending / demand": "Les dépenses / la demande des consommateurs",
+        "Janus-faced": "Ayant deux aspects",
+        "The bounds": "Les limites",
+        "Forestall": "Prévenir",
+        "Harbingers": "Signes avant-coureurs",
+        "To have your back to the wall": "Être dos au mur",
+        "To close shop": "Fermer boutique",
+        "A lame duck": "Un canard boiteux"
+      
+      
+  }
+  
+  
+  
+
+
+    function randomize(tab) {
+        var i, j, tmp;
+        for (i = tab.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            tmp = tab[i];
+            tab[i] = tab[j];
+            tab[j] = tmp;
+        }
+        return tab;}
+    
+    let zone_question = document.getElementById("question")
+    let bouton1 = document.getElementById('1')
+    let bouton2 = document.getElementById('2')
+    let bouton3 = document.getElementById('3')
+    let bouton4 = document.getElementById('4')
+    
+    
+    let scrore = document.getElementById('point')
+    let point = 0
+    let nombre = 0
+    var keys = []
+    for (const property in mot) {
+        keys.push(property)
+      }
+    var values = []
+    for (const property in mot) {
+          values.push(mot[property])
+        }
+    
+    function game(){
+        scrore.innerHTML=`${point}/${nombre}`
+        var rand = Math.floor(Math.random()*keys.length);
+    
+        var mot_de_la_question = keys[rand]
+        zone_question.innerHTML = mot_de_la_question
+    
+    
+        let run = true
+        while(run){
+            run = false
+            var rep = [
+                mot[mot_de_la_question],
+                values[Math.floor(Math.random()*keys.length)],
+                values[Math.floor(Math.random()*keys.length)],
+                values[Math.floor(Math.random()*keys.length)],
+            ]
+            for(let i in rep){
+                for (let j in rep){
+                    if(rep[i] == rep[j] && i != j){
+                        run = true
+                    }
+                }
+            }
+        }
+    
+        rep = randomize(rep)
+        bouton1.innerHTML = rep[0]
+        bouton2.innerHTML = rep[1]
+        bouton3.innerHTML = rep[2]
+        bouton4.innerHTML = rep[3]
+    }
+    function verif1(){
+        let reponse = zone_question.innerHTML
+        
+        if (bouton1.innerHTML == mot[reponse] ){point++}
+        nombre++
+        game()
+    }
+    function verif2(){
+        let reponse = zone_question.innerHTML
+        if (bouton2.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    function verif3(){
+        let reponse = zone_question.innerHTML
+        if (bouton3.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    function verif4(){
+        let reponse = zone_question.innerHTML
+        if (bouton4.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    
+    
+    
+    game()
+    bouton1.addEventListener("click",verif1)
+    bouton2.addEventListener("click",verif2)
+    bouton3.addEventListener("click",verif3)
+    bouton4.addEventListener("click",verif4)
+    
---
+++ mot_ch2FREN.js
@@ -0,0 +1,288 @@
+const mot = {
+    "Alimenter": "Supply",
+    "Usines": "Plants",
+    "Méandres": "Meander",
+    "Sevrer": "To wean",
+    "Étranger": "Foreign / from abroad",
+    "Rectiligne": "Straight",
+    "Des serres": "Greenhouses",
+    "Durable": "Sustainable",
+    "Craindre": "To fear",
+    "Fournir quelque chose à quelqu'un": "To provide somebody with",
+    "Dépendre": "Dependent",
+    "Réduire": "Cut down on",
+    "Susciter": "To arouse",
+    "Faire l’éloge de": "To praise",
+    "Donner naissance à": "Give birth to",
+    "Projet / Schéma": "Blueprint",
+    "Remettre à plus tard": "To postpone",
+    "La forme": "The shape",
+    "Actuellement": "Currently",
+    "Auto-suffisant": "Self-sufficient",
+    "Rencontrer": "To encounter",
+    "Sécheresse": "Draught",
+    "Pénurie": "Shortage",
+    "S’attaquer à un problème": "To address an issue",
+    "Concevoir": "To design",
+    "Sevrer quelqu'un de quelque chose": "To wean somebody off",
+    "Militants": "Activist",
+    "Rester": "Linger",
+    "S'enfoncer": "Sinking",
+    "Sous": "Beneath",
+    "S’effondrer": "Collapsing",
+    "Affaissement": "Subsidence",
+    "Changement": "Shift",
+    "Le support": "Bearing",
+    "Position basse": "Low-lying",
+    "Atténuation": "Mitigation",
+    "Côtière": "Coastal",
+    "La vase": "Silt",
+    "Évacuation": "Draining",
+    "L'économie britannique": "The British economy",
+    "L'économie (science)": "Economics",
+    "Le libéralisme": "Liberalism, free enterprise",
+    "Le libre-échange": "A free trade",
+    "Un partisan du libéralisme": "A free-marketeer",
+    "Une économie planifiée": "A planned economy",
+    "Une économie dirigée par l'État": "A state-run economy",
+    "Une entreprise publique": "A state-owned company",
+    "Intervenir": "To intervene",
+    "Le dirigisme": "State intervention(ism)",
+    "La loi de l'offre et la demande": "The law of supply and demand",
+    "Nationaliser": "To nationalize",
+    "Privatiser": "To privatize",
+    "Capitalisme": "Capitalism",
+    "Comprendre les choses": "Figure stuff out",
+    "Location": "Rental",
+    "Palette": "Bevy",
+    "Retour": "Feedback",
+    "Boucles de rétroaction": "Feedback loops",
+    "Possession": "Ownership",
+    "L'agriculture": "Agriculture / farming",
+    "Produire": "To produce",
+    "Un producteur": "A producer",
+    "Les produits agricoles": "Agricultural produce",
+    "Une culture": "A crop",
+    "Cultiver le blé/maïs": "To grow corn",
+    "L'agroalimentaire": "The food processing industry",
+    "L'agriculture biologique": "Organic farming",
+    "Les OGM": "GMOs / genetically modified organisms",
+    "Élever du bétail": "To breed cattle",
+    "L'élevage du bétail": "Cattle breeding",
+    "Un abattoir": "A slaughterhouse",
+    "La pêche": "Fishing",
+    "Une conserverie": "A fish factory",
+    "Récession / dépression / régression économique": "Recession / a slump",
+    "Déclencher": "Set off",
+    "S'effondrer": "Plummeted / to slump / to collapse / to plunge / to tumble",
+    "Salaires": "Wages",
+    "Entraîné": "Pulled down",
+    "Le secteur secondaire, l'industrie": "The manufacturing sector / the second sector",
+    "Une industrialisation": "An industrialization",
+    "La désindustrialisation": "Deindustrialization",
+    "Une usine": "A factory / plants",
+    "Atelier": "A workshop",
+    "Les matières premières": "Raw materials",
+    "Un contremaître": "A foreman",
+    "Fabriquer": "To manufacture",
+    "Fabriquer en série": "To mass produce",
+    "La production": "The output",
+    "Une machine-outil": "A machine tool",
+    "Faire fonctionner une machine": "To operate a machine",
+    "Respecter les normes de sécurité": "To meet safety standards",
+    "L'industrie lourde": "Heavy industry",
+    "L'industrie du textile": "The textile industry",
+    "La construction navale": "The shipbuilding industry",
+    "L'industrie pharmaceutique": "Pharmaceuticals",
+    "Un constructeur automobile": "A car maker, a car manufacturer",
+    "La construction automobile": "Car making",
+    "Automatiser": "To automate",
+    "Démanteler": "To dismantle",
+    "Les services, le tertiaire": "The service industries",
+    "Biens physiques": "Physical goods",
+    "Une entreprise": "A company / a firm",
+    "Une marque": "A make, a brand",
+    "Fonder / Monter une entreprise": "To set up a company",
+    "Diriger une entreprise": "To run a business",
+    "Les PME (Petites et Moyennes Entreprises)": "SMEs, small and medium-sized companies",
+    "Être en concurrence avec quelqu'un": "To compete with somebody",
+    "Un concurrent": "A competitor",
+    "La compétitivité": "Competitiveness",
+    "La concurrence déloyale": "Unfair competition",
+    "Accaparer un marché": "To corner a market",
+    "Percer un marché / Une part de marché": "To break into a market / a market share",
+    "Fusion d'entreprises": "A merger",
+    "Réaliser des bénéfices": "To make profit",
+    "Une perte": "A loss",
+    "Faire faillite": "To go bankrupt",
+    "Une dette": "Debt",
+    "Dont le noyau / l'essence": "Whose core",
+    "Si": "Whether",
+    "Politiques de gauche": "Left-wing policies",
+    "Exemple": "Case in point",
+    "Fervent": "A staunch",
+    "Une phase de baisse": "A downturn, a downswing",
+    "Baisser / Diminuer": "To fall / to slide / to decrease / to drop",
+    "Un ralentissement": "A slowdown",
+    "Connaître une période difficile": "To go through a bad patch",
+    "L'effondrement de l'économie mondiale": "The global economic meltdown",
+    "Rationaliser": "To streamline",
+    "Dégraisser ses effectifs": "To downsize",
+    "Restructuration": "Restructuring",
+    "Licencier des salariés": "To make workers redundant, to lay off workers",
+    "Supprimer des emplois": "To cut jobs",
+    "Perte d'emplois": "Job losses",
+    "Un aperçu": "A glimpse",
+    "Renaissant": "Resurgent",
+    "Envahissant": "Pervasive",
+    "Rarement": "Seldom",
+    "Foi": "Faith",
+    "Soutient": "Maintains",
+    "L'économie se redresse": "The economy is recovering",
+    "La reprise": "Recovery",
+    "Les affaires reprennent": "Business is picking up",
+    "Une remontée de la croissance": "An upswing in economic growth",
+    "Augmenter / Élever": "To increase / to go up / to rise",
+    "Monter en flèche": "To soar, to rocket",
+    "Un exportateur": "An exporter",
+    "La balance commerciale": "The balance of trade",
+    "Les chiffres du commerce extérieur": "Trade figures",
+    "Un déficit / Un excédent": "A deficit / a surplus",
+    "Tarifs douaniers": "Tariffs",
+    "Les barrières douanières": "Trade barriers",
+    "Une guerre commerciale": "A trade war",
+    "Tel que mesuré par pondération": "As measured by weighting",
+    "Étourdissant": "Dizzying",
+    "Balayé": "Swept",
+    "Est revenu": "Swept back",
+    "Optimiste": "Upbeat",
+    "Stimuler la croissance": "To boost growth",
+    "Des mesures d'austérité": "Austerity measures",
+    "Un plan de relance": "A stimulus plan / package",
+    "Un plan de sauvetage": "A rescue package",
+    "Déréglementer": "To deregulate",
+    "La déréglementation": "Deregulation",
+    "Renflouer une entreprise": "To bail out a company",
+    "Une subvention": "A subsidy",
+    "Subventionner": "To subsidize",
+    "La croissance": "Growth",
+    "Les dépenses publiques": "Public spending",
+    "Équilibrer un budget": "To balance a budget",
+    "Réduction budgétaire": "Drastic budget cuts",
+    "Un déficit budgétaire": "A budget deficit",
+    "Une agence de notation": "A credit-rating agency",
+    "Une augmentation des impôts": "A tax rise",
+    "L'investissement": "Investment",
+    "Les dépenses / la demande des consommateurs": "Consumer spending / demand",
+    "Ayant deux aspects": "Janus-faced",
+    "Les limites": "The bounds",
+    "Prévenir": "Forestall",
+    "Signes avant-coureurs": "Harbingers",
+    "Être dos au mur": "To have your back to the wall",
+    "Fermer boutique": "To close shop",
+    "Un canard boiteux": "A lame duck"
+}
+
+  
+  
+  
+
+
+    function randomize(tab) {
+        var i, j, tmp;
+        for (i = tab.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            tmp = tab[i];
+            tab[i] = tab[j];
+            tab[j] = tmp;
+        }
+        return tab;}
+    
+    let zone_question = document.getElementById("question")
+    let bouton1 = document.getElementById('1')
+    let bouton2 = document.getElementById('2')
+    let bouton3 = document.getElementById('3')
+    let bouton4 = document.getElementById('4')
+    
+    
+    let scrore = document.getElementById('point')
+    let point = 0
+    let nombre = 0
+    var keys = []
+    for (const property in mot) {
+        keys.push(property)
+      }
+    var values = []
+    for (const property in mot) {
+          values.push(mot[property])
+        }
+    
+    function game(){
+        scrore.innerHTML=`${point}/${nombre}`
+        var rand = Math.floor(Math.random()*keys.length);
+    
+        var mot_de_la_question = keys[rand]
+        zone_question.innerHTML = mot_de_la_question
+    
+    
+        let run = true
+        while(run){
+            run = false
+            var rep = [
+                mot[mot_de_la_question],
+                values[Math.floor(Math.random()*keys.length)],
+                values[Math.floor(Math.random()*keys.length)],
+                values[Math.floor(Math.random()*keys.length)],
+            ]
+            for(let i in rep){
+                for (let j in rep){
+                    if(rep[i] == rep[j] && i != j){
+                        run = true
+                    }
+                }
+            }
+        }
+    
+        rep = randomize(rep)
+        bouton1.innerHTML = rep[0]
+        bouton2.innerHTML = rep[1]
+        bouton3.innerHTML = rep[2]
+        bouton4.innerHTML = rep[3]
+    }
+    function verif1(){
+        let reponse = zone_question.innerHTML
+        
+        if (bouton1.innerHTML == mot[reponse] ){point++}
+        nombre++
+        game()
+    }
+    function verif2(){
+        let reponse = zone_question.innerHTML
+        if (bouton2.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    function verif3(){
+        let reponse = zone_question.innerHTML
+        if (bouton3.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    function verif4(){
+        let reponse = zone_question.innerHTML
+        if (bouton4.innerHTML == mot[reponse] ){point++}
+        
+        nombre++
+        game()
+    }
+    
+    
+    
+    game()
+    bouton1.addEventListener("click",verif1)
+    bouton2.addEventListener("click",verif2)
+    bouton3.addEventListener("click",verif3)
+    bouton4.addEventListener("click",verif4)
+    
